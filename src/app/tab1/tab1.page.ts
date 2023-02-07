ae8a353565ba8e2d716324da90733ffbeceab8f0
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2023-02-07 17:49:32 +0100

    Update tabs pages

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { babySister } from 'src/assets/profile.model';
+import { FavoriteService } from '../shared/favorite.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  babySister:babySister[];
+  isSelected: boolean = false;
+
+  constructor(private favoriteSrv: FavoriteService) {
+    this.babySister = [
+      {
+        userid:1,
+        firstName: "Chantal",
+        lastName: "Ayissa",
+        nombreMission: 2,
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore nulla reprehenderit error nobis in aspernatur.",
+        price:0.0,
+        photoProfile:"",
+        seddonime:"Mammy Chantal",
+        isLike: true
+      },
+      {
+        userid:2,
+        firstName: "Alida",
+        lastName: "Françoise",
+        nombreMission: 2,
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore nulla reprehenderit error nobis in aspernatur.",
+        price:0.0,
+        photoProfile:"",
+        seddonime:"Maa alida",
+        isLike: false,
+      }
+    ];
+  }
+
+  onAddToFavorite(userIndex: number){
+    this.babySister[userIndex].isLike = !this.babySister[userIndex].isLike;
+    this.favoriteSrv.addOrDelete(this.babySister[userIndex]);
+  }
 
 }
