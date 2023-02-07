e3c5d68e321231a7d1166fa40cce1bcb9520fcc5
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2023-02-07 17:50:01 +0100

    Create profile.model.ts

---
+++ src/assets/profile.model.ts
@@ -0,0 +1,11 @@
+export interface babySister{
+    userid?:number;
+    firstName: string;
+    lastName: string;
+    seddonime: string;
+    nombreMission: number;
+    description: string;
+    price:number;
+    photoProfile:string;
+    isLike:boolean;
+}
