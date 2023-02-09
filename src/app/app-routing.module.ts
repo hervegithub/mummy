5643031be587ea75268c067c4f659354b79553c6
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2023-02-09 08:05:56 +0100

    Update global css and app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'single-profile',
+    loadChildren: () => import('./single-profile/single-profile.module').then( m => m.SingleProfilePageModule)
   }
+
 ];
 @NgModule({
   imports: [
